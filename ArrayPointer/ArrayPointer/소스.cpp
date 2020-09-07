c2e6ec693923714d1e49fe9046ee1f39914c771a
zkem4wkd <69830656+zkem4wkd@users.noreply.github.com>
2020-09-07 15:09:13 +0900

    20.09.07

---
+++ ArrayPointer/ArrayPointer/소스.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*int iArray[3] = { 1,2,3 };
+
+	cout << iArray[0] << endl;
+	cout << iArray[1] << endl;
+	cout << iArray[2] << endl;
+
+	int i2Array[2][3] = { 0 };
+
+	cout << i2Array[0][0] << endl;
+	cout << i2Array[1][2] << endl;*/
+
+	//Pointer
+
+	/*int a = 10;
+	int* pA = &a;
+
+	*pA = 20;
+
+	cout << a << endl;*/
+
+	//int iArray[3] = { 6, 9 ,3 };
+	//int* pArray;
+
+	//pArray = iArray; //이름은 첫주소
+
+	//cout << *iArray << endl;
+	//cout << pArray << endl;
+
+	//printf("%#010x %#010x %#010x \n", iArray, &iArray[0], pArray);
+
+	/*int iArray[] = { 1,2,3 };
+
+	cout << iArray[0] << endl;
+	cout << iArray[1] << endl;
+	cout << iArray[2] << endl;*/
+
+
+	//동적할당
+	/*int* p;
+
+	p = new int[5];
+
+	p[0] = 1;
+	p[1] = 2;
+	p[2] = 3;
+	p[3] = 4;
+	p[4] = 5;
+
+	cout << "p[0] : " << p[0] << endl;
+	cout << "p[1] : " << p[1] << endl;
+	cout << "p[2] : " << p[2] << endl;
+	cout << "p[3] : " << p[3] << endl;
+	cout << "p[4] : " << p[4] << endl;
+	
+	delete []p;*/
+
+
+
+
+
+	return 0;
+}
