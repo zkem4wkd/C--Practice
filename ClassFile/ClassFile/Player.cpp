6eef78bbbe06ba905da408e6c3adf0cfb25e1d09
zkem4wkd <69830656+zkem4wkd@users.noreply.github.com>
2020-09-09 11:38:29 +0900

    20.09.09

---
+++ ClassFile/ClassFile/Player.cpp
@@ -0,0 +1,15 @@
+#include "Player.h"
+#include <iostream>
+using namespace std;
+
+Player::Player()
+{
+	att = 5;
+	def = 5;
+	cout << "Atk : " << att << "Def : " << def << endl;
+}
+
+Player::~Player()
+{
+
+}
--- ClassFile/ClassFile/소스.cpp
+++ ClassFile/ClassFile/소스.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include "Player.h"
 
 using namespace std;
 
 int main()
 {
+	Player p;
+
 
 
 
