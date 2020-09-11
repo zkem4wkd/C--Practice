359d8cc037e45cbe9abe2160143c83e2b88d31b6
zkem4wkd <69830656+zkem4wkd@users.noreply.github.com>
2020-09-11 16:41:59 +0900

    20.09.11

--- Virtual Function/Virtual Function/소스.cpp
+++ Virtual Function/Virtual Function/소스.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 using namespace std;
+//일반 가상함수 - 부모 클래스가 자식 클래스에게 기본적으로 물려주는 기능
+// 순수 가상함수 - 자식이 꼭 가지고 있어야 할 기능
 
 class Parent
 {
+
 public:
 	virtual void func() // virtual function
 	{
@@ -13,6 +16,7 @@ public:
 
 class Child : public Parent
 {
+
 	void func()
 	{
 		cout << "Child class function" << endl;
